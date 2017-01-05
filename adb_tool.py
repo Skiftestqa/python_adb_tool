8fe80578d06f843b9a1c255eb30fc69713d4114e
skiftestqa <skiftestqa@gmail.com>
2017-01-04 17:54:31 -0800

    update 2

--- adb_tool.py
+++ adb_tool.py
@@ -4,6 +4,7 @@ Tool in Python to capture video/screenshots/build installation. Works with pytho
 """
 
 import sys
+
 from datetime import datetime
 from subprocess import call
 
@@ -11,80 +12,104 @@ from subprocess import call
 def time_diff(start, finish):
     """Runtime Calculator"""
 
-    print round(finish, 2) - round(start, 2)
+    print "Run in %s" % (finish - start)
 
 
-#=== ADB commands for convenient system execution ===
-app_package = ""
-path_to_apk_on_PC = ""
-kill_server = 'adb kill-server >nul 2>&1'
-start_server = 'adb start-server'
-tcpip = 'adb tcpip 5555 >nul 2>&1'
-devices = 'adb devices'
-wake = 'adb shell input keyevent KEYCODE_WAKEUP >nul 2>&1'
-download = 'adb shell am start -a android.intent.action.VIEW -d "http://www.apk20.com/apk/187428/start" >nul 2>&1'
-uninstall = 'adb uninstall %s' % app_package
-install = 'adb install %s' % path_to_apk_on_PC
-notifications = 'adb shell service call statusbar 1 >nul 2>&1'
-clear_data = 'adb shell pm clear %s' % app_package
-start_time = 0
-end_time = 0
-total_run_time = 0
-exit_text = ""
-#====================================================
+def exit_script():
+    end_time = datetime.now()
+    time_diff(start_time, end_time)
+    exit_text = raw_input("Exit?(Y/N): ")
+    if exit_text.lower() == 'n':
+        sys.exit(0)
+    else:
+        pass
 
-
-#=== START MENU ===
+# === START MENU ===
 start_menu = """
 ================= Welcome========================
 Please choose an option below:
 0) Install build via USB connection
 1) Capture a screenshot
 2) Capture a video (Requires 4.4 firmware and up)
+-------------------------------------------------
 Type 'quit' to exit script.
+-------------------------------------------------
 """
 print start_menu
-#=================
-while True:
-    n = raw_input("Please input your selection: ")
-    while n == str(0):
-        start_time = datetime.now()
-        welcome_text_0 = """
-        ========================================================
-        === Welcome to the APK Installation Tool V.0.1 =========
-        ================ [Install Build] =======================
-        ========================================================
 
-        Connecting to device
-        """
-        print welcome_text_0
-        call(kill_server)
-        call(start_server)
-        call(devices)
-        Prompt_app_package = """
-        Please enter application package name at prompt
-        You can find this info in Settings=>App info=> Your app.
-        It should be named like com.example.apk
-        """
-        print Prompt_app_package
-        app_package = raw_input("Please enter application package name: ")
-        path_to_apk_on_PC = raw_input("Please enter full path to APK on PC"
-                                      "(it should look like'C:\Users\username\Downloads\example.apk'): ")
-        print "Clearing application data"
+selection = raw_input("Do you want to proceed?(Y/N): ")
+if selection.lower() == 'n' or 'quit':
+    sys.exit(0)
+elif selection.lower() == 'y':
+    pass
+
+# =================
+
+# =============== ADB commands aliases ==============================
+kill_server = 'adb kill-server'
+start_server = 'adb start-server'
+devices = 'adb devices'
+wake = 'adb shell input keyevent KEYCODE_WAKEUP'
+notifications = 'adb shell service call statusbar 1'
+start_time = 0
+end_time = 0
+total_run_time = 0
+exit_text = ""
+# ====================================================================
+
+
+n = raw_input("Please input your selection: ")
+if n == str(0):
+    start_time = datetime.now()
+    welcome_text_0 = """
+    ========================================================
+    === Welcome to the APK Installation Tool V.0.1 =========
+    ================ [Install Build via USB] ===============
+    ========================================================
+
+    Connecting to device
+    """
+    print welcome_text_0
+    call(kill_server)
+    call(start_server)
+    call(devices)
+    raw_input("Type package name you wish to work with at prompt."
+              "You can download an Package Reader app from Google"
+              "Play to find out name of the app package."
+              "Press Enter to proceed.")
+    app_package = raw_input("Please type in name of the package: ")
+    print "Clearing application data"
+    if app_package == call('adb shell pm list packages | grep %s' % app_package):
+        clear_data = 'adb shell pm clear %s' % app_package
         call(clear_data)
-        print "Uninstall any existing build"
-        call(uninstall)
-        print "APK will now install the build!"
-        call(wake)
-        call(install)
-        end_time = datetime.now()
-        time_diff(start_time, end_time)
-        raw_input("Type enter to exit")
-        sys.exit(0)
-    if n.lower() == 'quit':
-        sys.exit(0)
-
     else:
-        print "Please type in valid selection"
+        pass
+    print "Uninstall existing build"
+    if app_package == call('adb shell pm list packages | grep %s' % app_package):
+        uninstall = 'adb shell pm uninstall %s' %app_package
+        call(uninstall)
+    else:
+        pass
+    apk_path = raw_input("Please provide full path to APK you wish to install on your computer: ")
+    print "APK will now install the build!"
+    call(wake)
+    install = 'adb install %s' % apk_path
+    call(install)
+    exit_script()
 
+
+elif n == str(1):
+    while True:
+        screenshot_name = raw_input('Enter screenshot name. It will be amended with timestamp automatically: ')
+        screenshot_timestamped = screenshot_name + datetime.strftime(datetime.today(), "%Y_%m_%d-%H_%M_%S") + '.png'
+        call('adb shell screencap /sdcard/%s && adb pull /sdcard/%s' % screenshot_timestamped)
+        exit_screencap = raw_input("Finished taking screenshots?(Y/N): ")
+        if exit_screencap.lower() == 'y':
+            break
+        else:
+            continue
+elif n.lower() == 'quit':
+    sys.exit(0)
+else:
+    print "Please type in valid selection"
 
